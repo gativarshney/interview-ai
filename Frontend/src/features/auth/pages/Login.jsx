f75065a46c90a9bbb560d59d4878982903aec0b6
gativarshney <gativarshney2006@gmail.com>
2026-05-27 18:24:09 +0530

    frontend setup with login and register

---
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "../auth.form.scss"
+
+const Login = () => {
+  return (
+    <main>
+        <div className="form-container">
+            <h1>Login</h1>
+            <form>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" />
+                </div>
+                <button className='button primary-button'>Login</button>
+            </form>
+        </div>
+    </main>
+  )
+}
+
+export default Login
